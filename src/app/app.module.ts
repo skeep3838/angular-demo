03df89cd3d2bdae24cdb0b11d85388e3b334e5fb
NicoleLin <sleep3838@gmail.com>
2022-12-05 15:02:31 +0800

    3.1-3.2 單向繫結

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ch3ComponentComponent } from './ch3-component/ch3-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ch3ComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule { }
