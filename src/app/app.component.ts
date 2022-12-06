c9cc36da3a910f5622f9ec121484dc049aa25f3e
NicoleLin <sleep3838@gmail.com>
2022-12-06 11:23:24 +0800

    3.5 生命週期

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ValueConverter } from '@angular/compiler/src/render3/view/template';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Ch3ComponentComponent } from './ch3-component/ch3-component.component';
 
 @Component({
   selector: 'app-root',
@@ -14,5 +15,26 @@ export class AppComponent {
 
   onSizeChange(size: number): void {
     this.fontsizeFather = size;
+  }
+
+// 父元件在城史內使用子元件的屬性與方法，
+// 需利用 @ViewChild 裝飾器將子元件注入服元件內
+
+  // 參考變數名稱
+  @ViewChild('fontsize') fontsize2!: Ch3ComponentComponent;
+  // 指定元件名稱
+  @ViewChild(Ch3ComponentComponent) ch3ComponentComponent!: Ch3ComponentComponent;
+  // HTML 元素實體
+  @ViewChild('firstDiv') divElement!: ElementRef;
+
+  ngAfterViewInit():void{
+    console.log("ch3-component 6. ngAfterViewInit ##");
+  }
+
+  ngAfterViewChecked():void{
+    console.log("ch3-component 7. ngAfterViewChecked ##");
+    console.log(this.fontsize2);
+    console.log(this.ch3ComponentComponent);
+    console.log(this.divElement);
   }
 }
--- src/app/ch3-component/ch3-component.component.ts
+++ src/app/ch3-component/ch3-component.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Attribute, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Attribute, Output, EventEmitter, OnChanges, SimpleChanges, ContentChild, ElementRef, ContentChildren, QueryList} from '@angular/core';
 
 @Component({
   selector: 'app-ch3-component',
   templateUrl: './ch3-component.component.html',
   styleUrls: ['./ch3-component.component.css']
 })
-export class Ch3ComponentComponent implements OnInit {
+export class Ch3ComponentComponent implements OnInit, OnChanges {
 
   constructor(@Attribute('color') public color: string) { }
 
@@ -16,11 +16,10 @@ export class Ch3ComponentComponent implements OnInit {
   fontColor = "red";
   fontSizeType = "middle";
 
-  ngOnInit(): void {
-  }
-
   // 3.3.1 元間屬性允許從父元件接收資料
   @Input() size!: number;
+  @Input() font!: number;
+
   private _sizePriv!: number;
 
   @Input()
@@ -29,8 +28,9 @@ export class Ch3ComponentComponent implements OnInit {
   }
 
   @Output() sizeChange = new EventEmitter<number>();
+  @Output() sizeChangeStr = new EventEmitter<string>();
 
-  get sizePriv(){
+  get sizePriv() {
     return this._sizePriv.toString();
   }
 
@@ -43,6 +43,58 @@ export class Ch3ComponentComponent implements OnInit {
   // 3.2.5 類別繫結
   get fontClass(): string {
     return `${this.fontSizeType}-font`;
+  }
+
+  // SimpleChanges 可以確認是否首次變更 - 待確認
+  ngOnChanges(changes: SimpleChanges): void {
+    // 如果觀察的值變更後不會改道Component內的參考位置
+    // 則不會觸發ngChanges
+    console.log(changes['font']);
+
+    console.log("ch3-component 1. ngOnChanges ##");
+    //  if (changes['size']) {
+    //   console.log("size: "+ this.size);
+    //   this.sizeChangeStr.emit(`${changes['size'].currentValue}pt`)
+    // }
+  }
+
+  // 元件初始化，整個生命週期只會呼叫一次
+  ngOnInit(): void {
+    console.log("ch3-component 2. ngOnInit ##");
+  }
+
+  // 如果觀察的值變更後不會改道Component內的參考位置
+  // 則不會觸發ngChanges
+  // 這種狀況可以使用ngDoCheck 檢查邏輯
+  ngDoCheck() {
+    console.log("ch3-component 3. ngDoCheck ##");
+  }
+
+  // 單個元件實體
+  @ContentChild('title') titleElement!: ElementRef;
+  // 多個元件實體
+  @ContentChildren('button', { descendants: true }) bottonelements!: QueryList<ElementRef>;
+  // 加入@ContentChild 裝飾器屬性，會在 ngAfterContentInit 依條件取得元件實體
+  // 並在 ngDoCheck 後觸發，只會觸發一次
+  ngAfterContentInit() {
+    console.log("ch3-component 4. ngAfterContentInit ##");
+    console.log(this.titleElement);
+  }
+
+  // 變更檢測時，在 ngDoCheck 後觸發
+  ngAfterContentChecked() {
+    console.log("ch3-component 5. ngAfterContentChecked ##");
+    console.log(this.titleElement);
+    console.log(this.bottonelements);
+  }
+
+//   使用者離開應用程式會觸發 ngOnDestory ，
+//   釋放不會被自動收回的垃圾，避免記憶體洩漏風險，包含：
+// 1. DOM 事件或可監控物件的取消訂閱
+// 2. interval 計時器的停止
+// 3. 「取消註冊」此指令所註冊過的callback方法
+  ngOnDestory(){
+    console.log("ch3-component 8. ngOnDestory ##");
   }
 
 }
